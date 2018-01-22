c59adb334ab86daf44ee96cd68b632efd9d03bf9
Oliver Diamond <oliver.diamond@tesglobal.com>
2018-01-22 08:07:41 +0000

    Separate Day Component

--- ui/components/DataSection.jsx
+++ ui/components/DataSection.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import Plot from 'react-plotly.js'
-import accounting from 'accounting';
-import moment from 'moment';
 
-const formatDate = date => moment(date).format('DD-MMM-YYYY');
-const formatCurrency = num => accounting.formatMoney(num/100, '£');
+import Day from './Day.jsx';
 
-const log = (date, transactions) => {
-  console.log('=========');
-  console.log(formatDate(date));
-  transactions.forEach(transaction => {
-    console.log('('+formatCurrency(transaction.amount)+')', transaction.description);
-  })
-}
 
 const blankRow = (
   <tr>
@@ -26,20 +16,10 @@ const blankRow = (
   </tr>
 );
 
-const createRow = row => (
-  <tr key={row.date} >
-    <td>{formatDate(row.date)}</td>
-    <td style={row.spending < -50000 ? {color: '#F00'} : {}} onClick={() => log(row.date, row.transactions.spending)}>{formatCurrency(row.spending)}</td>
-    <td onClick={() => log(row.date, row.transactions.income)}>{formatCurrency(row.income)}</td>
-    <td onClick={() => log(row.date, row.transactions.rent)}>{formatCurrency(row.rent)}</td>
-    <td>{formatCurrency(row.balance)}</td>
-    <td>{formatCurrency(row.amortisedBalance)}</td>
-    <td>{formatCurrency(row.spendingSoFarThisMonth)}</td>
-  </tr>
-);
-
 const DataSection = ({data}) => {
-  const rows = data ? data.reverse().map(createRow) : blankRow;
+  const rows = data ? data.reverse().map(row => (
+    <Day key={row.date} {...row} />
+  )) : blankRow;
 
   return (
     <div>
---
+++ ui/components/Day.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import accounting from 'accounting';
+import moment from 'moment';
+
+const formatDate = date => moment(date).format('DD-MMM-YYYY');
+const formatCurrency = num => accounting.formatMoney(num/100, '£');
+
+const log = (date, transactions) => {
+  console.log('=========');
+  console.log(formatDate(date));
+  transactions.forEach(transaction => {
+    console.log('('+formatCurrency(transaction.amount)+')', transaction.description);
+  })
+};
+
+const Day = row => (
+  <tr>
+    <td>{formatDate(row.date)}</td>
+    <td style={row.spending < -50000 ? {color: '#F00'} : {}} onClick={() => log(row.date, row.transactions.spending)}>{formatCurrency(row.spending)}</td>
+    <td onClick={() => log(row.date, row.transactions.income)}>{formatCurrency(row.income)}</td>
+    <td onClick={() => log(row.date, row.transactions.rent)}>{formatCurrency(row.rent)}</td>
+    <td>{formatCurrency(row.balance)}</td>
+    <td>{formatCurrency(row.amortisedBalance)}</td>
+    <td>{formatCurrency(row.spendingSoFarThisMonth)}</td>
+  </tr>
+);
+
+module.exports = Day;

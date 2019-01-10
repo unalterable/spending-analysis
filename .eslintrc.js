47733dac90c207e40565be83610f9ce959963995
Oliver Diamond <oliver.diamond@sky.uk>
2019-01-10 09:42:12 +0000

    linting plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,32 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "mocha": true,
+  'env': {
+    'node': true,
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'mocha': true,
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
-    },
-    "sourceType": "module"
+  'plugins': ['react'],
+  'extends': ['eslint:recommended', 'plugin:react/recommended'],
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'ecmaFeatures': { 'jsx': true },
+    'sourceType': 'module'
   },
-  "plugins": [
-    "react",
-  ],
-  "rules": {
-    "no-console": [2, { "allow": ["info", "error"] }],
-    "react/jsx-uses-react": 1,
-    "react/jsx-uses-vars": 1,
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
-    "comma-dangle": ["error", {
-      "arrays": "always-multiline",
-      "objects": "always-multiline",
-      "imports": "always-multiline",
-      "exports": "always-multiline",
-      "functions": "ignore"
+  'rules': {
+    'no-console': [2, { 'allow': ['info', 'error'] }],
+    'react/jsx-uses-react': 1,
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'always-multiline',
+      'exports': 'always-multiline',
+      'functions': 'ignore'
     }],
   },
 };
--- src/ui/Application.jsx
+++ src/ui/Application.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Theme from './Theme.jsx';
 import TopNav from './components/TopNav.jsx';
 

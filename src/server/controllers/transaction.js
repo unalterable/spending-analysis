17940e39d4f87b2ad7abd213b9c79df088254f19
nes11 <agnesgaroux47@gmail.com>
2019-02-17 19:48:51 +0000

    added check before saving new transactions

--- src/server/controllers/transaction.js
+++ src/server/controllers/transaction.js
@@ -1,18 +1,29 @@
 const analysis = require('../analysis/index');
 
 const initTransactionController = (store) => {
+  const _performChecks = async ({ userId, newTransactions }) => {
+    const [statement, rentStrings] = await Promise.all([store.transactions.getAll(userId), store.rentStrings.getAll(userId)]);
+    await analysis({ statement: statement.concat(newTransactions), rentStrings: rentStrings.map(({ string }) => string)  });
+  };
+
   const data = async (req, res) => {
     const userId = req.jwt.user.id;
     const [statement, rentStrings] = await Promise.all([store.transactions.getAll(userId), store.rentStrings.getAll(userId)]);
-    const json = await analysis({ statement, rentStrings: rentStrings.map(({string}) => string) });
+    const json = await analysis({ statement, rentStrings: rentStrings.map(({ string }) => string) });
     res.send(json);
   };
 
   const saveTransactions = async (req, res) => {
-    const userId = req.jwt.user.id;
-    const newData = [].concat(req.body);
-    await store.transactions.insertMany(userId, newData);
-    res.sendStatus(204);
+    try {
+      const userId = req.jwt.user.id;
+      const newData = [].concat(req.body);
+      await _performChecks({ userId, newTransactions: newData });
+      await store.transactions.insertMany(userId, newData);
+      res.sendStatus(204);
+    } catch (e) {
+      res.sendStatus(409);
+      console.error('saveTransactions error =>', e);
+    }
   };
 
   return {
